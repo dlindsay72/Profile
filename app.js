b8f16bf09ecd4ef1705989d1bf8b629814357b28
Dan Lindsay <dlindsay72@hotmail.com>
2018-01-15 18:52:25 -0500

    put all the profile code into profile.js and exported the module to app.js

--- app.js
+++ app.js
@@ -1,51 +1,6 @@
-//Require for https module
-const https = require('https');
-//Require http module for status codes
-const http = require('http');
-
-//Print error messages
-function printError(error) {
-    console.error(error.message);
-}
-
-function printMessage(userName, badgeCount, point) {
-    const message = `${userName} has ${badgeCount} total badge(s) and ${point} points in JavaScript`;
-    console.log(message)
-}
-function getProfile(username) {
-    try {
-        const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
-            if (response.statusCode === 200) {
-                let body = "";
-                //Read the data
-                response.on('data', data => {
-                    body += data.toString();
-                });
-
-                response.on('end', () => {
-                    //Parse the data
-                    //Print the data
-                    try {
-                        const profile = JSON.parse(body);
-                        printMessage(username, profile.badges.length, profile.points.JavaScript);
-                    } catch (error) {
-                        printError(error);
-                    }
-                });
-            } else {
-                const message = `There was an error getting the profile for ${username} (${http.STATUS_CODES[response.statusCode]})`;
-                const statusCodeError = new Error(message);
-                printError(statusCodeError);
-            }
-        });
-
-        request.on('error', printError);
-    } catch (error) {
-        printError(error);
-    }
-}
+const profile = require('./profile.js');
 // Connect to the API URL (https://teamtreehouse.com/danlindsay.json)
 const users = process.argv.slice(2);
 
-users.forEach(getProfile);
+users.forEach(profile.getrdone);
 
---
+++ profile.js
@@ -0,0 +1,48 @@
+//Require for https module
+const https = require('https');
+//Require http module for status codes
+const http = require('http');
+
+//Print error messages
+function printError(error) {
+    console.error(error.message);
+}
+
+function printMessage(userName, badgeCount, point) {
+    const message = `${userName} has ${badgeCount} total badge(s) and ${point} points in JavaScript`;
+    console.log(message)
+}
+function get(username) {
+    try {
+        const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
+            if (response.statusCode === 200) {
+                let body = "";
+                //Read the data
+                response.on('data', data => {
+                    body += data.toString();
+                });
+
+                response.on('end', () => {
+                    //Parse the data
+                    //Print the data
+                    try {
+                        const profile = JSON.parse(body);
+                        printMessage(username, profile.badges.length, profile.points.JavaScript);
+                    } catch (error) {
+                        printError(error);
+                    }
+                });
+            } else {
+                const message = `There was an error getting the profile for ${username} (${http.STATUS_CODES[response.statusCode]})`;
+                const statusCodeError = new Error(message);
+                printError(statusCodeError);
+            }
+        });
+
+        request.on('error', printError);
+    } catch (error) {
+        printError(error);
+    }
+}
+
+module.exports.getrdone = get;
